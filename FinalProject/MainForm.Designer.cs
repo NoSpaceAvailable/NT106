dd49d18c8015e80b88980914f6339183b8a0879f
anzuukino <songokult1@gmail.com>
2025-04-10 02:39:55 +0700

    Adding cursor and color menu

--- FinalProject/MainForm.Designer.cs
+++ FinalProject/MainForm.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace FinalProject
+﻿using System.Windows.Forms;
+
+namespace FinalProject
 {
     partial class MainForm
     {
@@ -28,23 +30,64 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panelColors = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panelColors
+            // 
+            this.panelColors.Location = new System.Drawing.Point(56, 39);
+            this.panelColors.Name = "panelColors";
+            this.panelColors.Size = new System.Drawing.Size(596, 100);
+            this.panelColors.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 154);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1580, 722);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // trackBar
+            // 
+            this.trackBar.AutoSize = false;
+            this.trackBar.CausesValidation = false;
+            this.trackBar.Location = new System.Drawing.Point(751, 71);
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Size = new System.Drawing.Size(249, 56);
+            this.trackBar.TabIndex = 2;
+            this.trackBar.TickStyle = System.Windows.Forms.TickStyle.None;
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(1185, 712);
+            this.ClientSize = new System.Drawing.Size(1580, 876);
+            this.Controls.Add(this.trackBar);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panelColors);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Advanced Paint";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelColors;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private TrackBar trackBar;
     }
 }
 
--- FinalProject/MainForm.cs
+++ FinalProject/MainForm.cs
@@ -8,13 +8,133 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace FinalProject
 {
     public partial class MainForm: Form
     {
+        private Color selectedColor = Color.Black;
+        private Button customcoButton; // Button for custom color selection like you can type hex value on that and change color to that value
+        private int penWidth = 10;
         public MainForm()
         {
             InitializeComponent();
+            LoadColorPalette();
+            SetupBrushSizeMenu();
+            UpdateCursor();
         }
+
+        private void SetupBrushSizeMenu()
+        {
+            trackBar.Minimum = 1;
+            trackBar.Maximum = 30;
+            trackBar.Value = penWidth;
+            trackBar.Width = 150;
+
+            trackBar.Scroll += (s, e) =>
+            {
+                penWidth = trackBar.Value;
+                UpdateCursor();
+            };
+
+            Controls.Add(trackBar);
+
+
+        }
+
+        private void UpdateCursor()
+        {
+            int size = penWidth;
+            Bitmap bmp = new Bitmap(size, size);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                g.Clear(Color.Transparent);
+
+                using (Brush brush = new SolidBrush(selectedColor))
+                {
+                    g.FillEllipse(brush, 0, 0, size - 1, size - 1);
+                }
+
+   
+                using (Pen pen = new Pen(Color.Black, 1))
+                {
+                    g.DrawEllipse(pen, 0, 0, size - 1, size - 1);
+                }
+            }
+
+            // Set custom cursor
+            Cursor customCursor = new Cursor(bmp.GetHicon());
+            pictureBox1.Cursor = customCursor;
+        }
+
+
+        private void LoadColorPalette()
+        {
+            Color[] colors = new Color[]
+            {
+                Color.Black, Color.White, Color.Gray, Color.Red, Color.Maroon,
+                Color.Orange, Color.Yellow, Color.Green, Color.Blue, Color.Purple,
+                Color.Brown, Color.Pink, Color.LightGreen, Color.LightBlue, Color.Violet
+            };
+
+            int buttonSize = 30;
+            for (int i = 0; i < colors.Length; i++)
+            {
+                Button colorBtn = new Button();
+                colorBtn.BackColor = colors[i];
+                colorBtn.Width = buttonSize;
+                colorBtn.Height = buttonSize;
+                colorBtn.Left = (i % 8) * (buttonSize + 5);
+                colorBtn.Top = (i / 8) * (buttonSize + 5);
+                colorBtn.FlatStyle = FlatStyle.Flat;
+                colorBtn.FlatAppearance.BorderSize = 1;
+                colorBtn.Tag = colors[i];
+                colorBtn.Click += ColorBtn_Click;
+
+                colorBtn.MouseEnter += ColorBtn_MouseEnter;
+                colorBtn.MouseLeave += ColorBtn_MouseLeave;
+
+                panelColors.Controls.Add(colorBtn);
+
+                if (i == colors.Length - 1)
+                {
+                    customcoButton = colorBtn;
+                    customcoButton.Click += customcoButton_Click;
+                }
+            }
+        }
+
+
+        private void ColorBtn_MouseEnter(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Hand;
+        }
+
+        private void ColorBtn_MouseLeave(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Default;
+        }
+
+        private void customcoButton_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                customcoButton.BackColor = colorDialog.Color;
+                customcoButton.Tag = colorDialog.Color;
+                selectedColor = colorDialog.Color;
+                UpdateCursor();
+            }
+        }
+
+
+        private void ColorBtn_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = sender as Button;
+            selectedColor = (Color)clickedButton.Tag;
+            UpdateCursor();
+        }
+        
     }
 }
